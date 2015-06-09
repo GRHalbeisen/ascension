464b27ffb5147b3daa2493f2c07c4f6f8fc28008
GRHalbeisen <grhalbeisen@gmail.com>
2015-06-08 20:31:16 -0500

    navmesh click to move

---
+++ Ascension/Assets/Scripts/Player/ClickToMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start ClickToMove");
+		navAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if(Input.GetMouseButtonUp(0))
+		{
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				Debug.Log(hit.point);
+				navAgent.SetDestination(hit.point);
+			}
+			else 
+			{
+
+			}
+		}
+	}
+
+
+}
