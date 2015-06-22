19410a591487674d8d83145e10e0b56c507f5ace
GRHalbeisen <grhalbeisen@gmail.com>
2015-06-22 02:56:10 -0500

    warp btw areas example, camera setup, isometric view, and player prefab created

---
+++ Ascension/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private static Transform player;
+	private static Vector3 relCameraPos;
+	private float relCameraPosMag;
+	private Vector3 newPos;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag (tag:"Player").transform;
+		relCameraPos = transform.position - player.position;
+	}
+
+	public void UpdateModeCameraPos(Vector3 destination)
+	{
+		//transform.position = player.position + relCameraPos;
+		Camera.main.transform.position = destination + relCameraPos;
+	}
+}
---
+++ Ascension/Assets/Scripts/Environment/DoorController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+	public Transform destination;
+	public CameraMovement cm;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Player entered trigger.");
+		Vector3 dest = new Vector3 (destination.position.x, destination.position.y, destination.position.z);
+		//other.gameObject.transform.position = dest;
+		other.GetComponent<NavMeshAgent> ().Warp (dest);
+//		Camera.main.transform = dest;
+
+		cm.UpdateModeCameraPos (destination.position);
+	}
+}
